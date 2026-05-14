7888f05ea8ba7675b55ff0b696d967bf72240e0d
Nisha <nishavahutre001@gmail.com>
2026-05-14 17:46:23 +0530

    Setup config.py with pydantic settings for env

---
+++ app/config.py
@@ -0,0 +1,8 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    Database_url :str
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+settings = Settings()
